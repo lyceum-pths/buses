b11b3601afa542ecdb93affa2adfaab51111a1d2
Artem Shalagin <artyom.shalagin@gmail.com>
2015-02-24 21:31:21 +0300

    small changes

--- src/ru/ioffe/school/buses/gui/GUIControl.java
+++ src/ru/ioffe/school/buses/gui/GUIControl.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -114,6 +116,21 @@ public class GUIControl extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				model.moveUp(20);
 				mapPanel.repaint();
+			}
+		});
+		upButton.addKeyListener(new KeyListener() {
+			public void keyTyped(KeyEvent e) {
+			}
+			public void keyReleased(KeyEvent e) {
+				
+			}
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					model.moveUp(20);
+					mapPanel.repaint();
+				}
+				System.out.println(KeyEvent.VK_UNDERSCORE);
+				
 			}
 		});
 		downButton = new JButton("Down");

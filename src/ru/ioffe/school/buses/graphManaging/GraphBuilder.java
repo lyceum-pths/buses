f5db4f086c4b77c834a36f9e2a268b2c05f23324
ArtyomLobanov <lobanov98@yandex.ru>
2015-03-16 22:21:45 +0300

    Теперь GraphBuilder умеет искать компоненты связности

--- src/ru/ioffe/school/buses/graphManaging/GraphBuilder.java
+++ src/ru/ioffe/school/buses/graphManaging/GraphBuilder.java
@@ -1,6 +1,8 @@
 package ru.ioffe.school.buses.graphManaging;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import ru.ioffe.school.buses.data.Point;
@@ -21,11 +23,15 @@ public class GraphBuilder {
 	}
 
 	public Road[] getRoads() {
-		Road[] res = new Road[roads.size()];
-		int pos = 0;
-		for (Road road : roads) 
-			res[pos++] = road;
-		return res;
+		return roads.toArray(new Road[roads.size()]);
+	}
+	
+	public void addRoad(Road road) {
+		roads.add(road);
+	}
+	
+	public int size() {
+		return roads.size();
 	}
 
 	/**
@@ -93,5 +99,99 @@ public class GraphBuilder {
 		double vectorProduct = Math.abs((b.getX() - a.getX()) * (point.getY() - a.getY()) -
 				(b.getY() - a.getY()) * (point.getX() - a.getX()));
 		return vectorProduct / road.getLength();
+	}
+
+	public GraphBuilder[] separateGraph() {
+		SNM snm = new SNM();
+		for (Road road : roads) 
+			snm.merge(road.getFrom(), road.getTo());
+		HashMap<Integer, Integer> map = new HashMap<>();
+		GraphBuilder[] graphs = new GraphBuilder[snm.size];
+		for (int i = 0; i < snm.size; i++)
+			graphs[i] = new GraphBuilder();
+		int iterator = 0;
+		int group; 
+		Integer index;
+		for (Road road : roads) {
+			group = snm.get(road.getFrom());
+			index = map.get(group);
+			if (index == null) {
+				index = iterator++;
+				map.put(group, index);
+			}
+			graphs[index].addRoad(road);
+		}
+		return graphs;
+	}
+	
+	public GraphBuilder findMaxComponent() {
+		GraphBuilder[] comps = separateGraph();
+		int maxIndex = 0;
+		if (comps.length == 0)
+			throw new IllegalArgumentException("Graph empty!");
+		for (int i = 1; i < comps.length; i++) {
+			if (comps[maxIndex].size() < comps[i].size())
+				maxIndex = i;
+		}
+		return comps[maxIndex];
+	}
+
+	private static class SNM {
+		ArrayList<Integer> pred;
+		ArrayList<Integer> rang;
+		HashMap<Point, Integer> map;
+		int size;
+
+		public SNM() {
+			pred = new ArrayList<>();
+			rang = new ArrayList<>();
+			map = new HashMap<>();
+			size = 0;
+		}
+
+		public int get(int n) {
+			if (pred.get(n) == n) {
+				return n;
+			}
+			pred.set(n, get(pred.get(n)));
+			return pred.get(n);
+		}
+
+		public int get(Point p) {
+			return get(find(p));
+		}
+
+		public int find(Point p) {
+			Integer index = map.get(p);
+			if (index != null)
+				return index;
+			size++;
+			index = pred.size();
+			pred.add(index);
+			rang.add(0);
+			map.put(p, index);
+			return index;
+		}
+
+		public void merge(Point a, Point b) {
+			merge(find(a), find(b));
+		}
+
+		public void merge(int i, int j) {
+			i = get(i);
+			j = get(j);
+			if (i == j) {
+				return;
+			}
+			if (rang.get(i) < rang.get(j)) {
+				int help = i;
+				i = j;
+				j = help;
+			}
+			pred.set(j, i);
+			size--;
+			if (rang.get(i) == rang.get(j))
+				rang.set(i, Math.max(rang.get(i), rang.get(j) + 1));
+		}
 	}
 }

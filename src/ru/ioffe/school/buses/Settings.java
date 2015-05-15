18e3ad3c91b6f8d0404f2824b4d99ae37e614c53
ArtyomLobanov <lobanov98@yandex.ru>
2015-05-15 21:36:07 +0300

    Подгоняем потихоньку

--- src/ru/ioffe/school/buses/Settings.java
+++ src/ru/ioffe/school/buses/Settings.java
@@ -3,7 +3,7 @@ package ru.ioffe.school.buses;
 public class Settings {
 	public static double PERSONS_SPEED = 5;
 	public static int[] BUSES = {2, 2, 3, 3, 4};
-	public static double MAGIC_TIME_GENERATION = 1.0005;
+	public static double MAGIC_TIME_GENERATION = 1.0003;
 	public static double NIGHT_LENGTH = 21600;
 	
 }
